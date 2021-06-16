56aa9b8bec84dd5a8e85c3fcdd1bd2bb0ccaecba
Charles <charlescampet03@gmail.com>
2021-06-16 16:39:26 +0200

    set boolean at false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_131831) do
+ActiveRecord::Schema.define(version: 2021_06_16_143028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_131831) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "is_manager"
+    t.boolean "is_manager", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
