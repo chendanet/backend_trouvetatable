8d336f1ffcb03ecf5bfa326a82706256da04475f
Janainaf <figueira.janaina@gmail.com>
2021-06-16 18:27:35 +0200

    adding keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
 
 		def configure_permitted_parameters
 
-			devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :is_manager )}
+			devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, keys: [:is_manager] )}
 
 			devise_paramater_sanitizer.permit(:account_update) { |u| u.permit( :email, :password, :is_manager, :first_name, :last_name)}
 
