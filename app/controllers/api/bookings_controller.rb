ab0e74e2c49f1e95b9a4a5437b74875c52012112
Janainaf <figueira.janaina@gmail.com>
2021-06-17 22:31:54 +0200

    fixed typos

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -4,40 +4,45 @@ class Api::BookingsController < Api::BaseController
         @bookings = Booking.all
     
         render json: @bookings
-      end
+    end
 
-      def show
+    def show
         render json: @booking
-      end
+    end
 
-      def create
-        @user = current_user        
+    def create
+        @user = current_user    
+
         @booking = Booking.new(
             seat: booking_params[:seat], 
             time: booking_params[:time], 
             date: booking_params[:date], 
-            venue_id: booking_params[:venue_ud], 
+            venue_id: booking_params[:venue_id], 
             user_id: @user.id, 
-            
-# Il faut reflechir comment faire le venue_id
-    
+        )
+ 
         if @booking.save
-                render json: @booking, status: :created, location: @api_booking
+            render json: @booking, status: :created
         else
-                render json: @booking.errors, status: :unprocessable_entity
-              end
-            end
+            render json: @booking.errors, status: :unprocessable_entity
+        end    
+     end
 
-            def update
-                if @booking.update(booking_params)
-                  render json: @booking
-                else
-                  render json: @booking.errors, status: :unprocessable_entity
-                end
-              end
+    def update
+        if @booking.update(booking_params)
+            render json: @booking
+         else
+            render json: @booking.errors, status: :unprocessable_entity
+        end
+    end
 
-              def destroy
-                @booking.destroy
-              end
-            
+     def destroy
+          @booking.destroy
+     end
+
+
+     def booking_params
+        params.permit(:seat, :time, :date, :venue_id, :user_id)
+      end
+
 end
