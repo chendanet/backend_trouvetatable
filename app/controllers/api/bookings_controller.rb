1d89515269d14a13a510db7b745af9bf56bcd345
Janainaf <figueira.janaina@gmail.com>
2021-06-18 08:18:06 +0200

    fixing code

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,24 +1,27 @@
 class Api::BookingsController < Api::BaseController
+    # before_action :set_booking, only: [:show, :update, :destroy]
+    # before_action :authenticate_user!, only: [:create, :update, :destroy]
+    # before_action :is_owner , only: [:edit, :update, :destroy]
 
     def index
         @bookings = Booking.all
     
-        render json: @bookings
+        render json: @bookings.to_json
     end
 
     def show
-        render json: @booking
+        render json: @booking.to_json
     end
 
     def create
-        @user = current_user    
+        # @user = current_user    
 
         @booking = Booking.new(
             seat: booking_params[:seat], 
             time: booking_params[:time], 
             date: booking_params[:date], 
             venue_id: booking_params[:venue_id], 
-            user_id: @user.id, 
+            # user_id: @user.id, 
         )
  
         if @booking.save
@@ -40,9 +43,22 @@ class Api::BookingsController < Api::BaseController
           @booking.destroy
      end
 
+    private
+
+    def set_booking
+        @booking = Booking.find(params[:id])
+      end
 
      def booking_params
         params.permit(:seat, :time, :date, :venue_id, :user_id)
+      end
+
+      def is_owner 
+        @booking= Booking.find(params[:id])
+        unless @booking.user == current_user
+          render json: nil, status: :forbidden
+          return
+        end
       end
 
 end
